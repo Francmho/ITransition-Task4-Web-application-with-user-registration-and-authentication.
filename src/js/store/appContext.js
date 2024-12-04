a2e61ba3689dafc4e326466e59cb033437c07213
Franco Morales <73557750+Francmho@users.noreply.github.com>
2024-12-04 05:28:00 +0000

    Added isLoggedIn  management to appContext.js

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -19,12 +19,28 @@ const injectContext = PassedComponent => {
 			})
 		);
 
+
+		// State for user login
+        const [isLoggedIn, setIsLoggedIn] = useState(false);
+        const [user, setUser] = useState(null);
+
+		// Functions for login management
+        const logIn = (userData) => {
+            setIsLoggedIn(true);
+            setUser(userData);
+        };
+
+		const logOut = () => {
+            setIsLoggedIn(false);
+            setUser(null);
+        };
+
 		useEffect(() => {
 		}, []);
 
 
 		return (
-			<Context.Provider value={state}>
+			<Context.Provider value={{ ...state, isLoggedIn, user, logIn, logOut }}>
 				<PassedComponent {...props} />
 			</Context.Provider>
 		);
