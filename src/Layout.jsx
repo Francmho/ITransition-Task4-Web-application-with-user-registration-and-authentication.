89c4cabb50202102effe9190429548ec6dc67986
Franco Morales <73557750+Francmho@users.noreply.github.com>
2024-12-06 00:15:30 +0000

    cdns bootstrap in index & loginregister renamed register

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './views/Home.jsx';
 import Contact from './views/Contact.jsx';
 import NotFound from './views/NotFound.jsx';
-import LoginRegister from './views/LoginRegister.jsx';
+import Register from './views/Register.jsx';
 import Login from './views/Login.jsx';
 import injectContext from './js/store/appContext.js';
 
@@ -13,7 +13,7 @@ const Layout = () => {
     <div>
         <BrowserRouter basename={basename}>
             <Routes>
-                <Route exact path='/' element={<LoginRegister/>}/>
+                <Route exact path='/' element={<Register/>}/>
                 <Route exact path='/login' element={<Login />} />
                 <Route exact path='/home' element={<Home/>}/>
                 <Route exact path='/contact' element={<Contact/>}/>
